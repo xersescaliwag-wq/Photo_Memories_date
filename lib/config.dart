6919954c8a90122f9bf23a6b47742cd31cecec83
angelapereyra0731-a11y <angelapereyra0731@gmail.com>
2026-09-14 19:38:48 +0800

    Update config.dart
    Add configurable server IP

--- lib/config.dart
+++ lib/config.dart
@@ -1,18 +1,35 @@
 class ApiConfig {
-  // ngrok (public):    useNgrok = true   -> https://xxxx.ngrok-free.dev
-  // Same WiFi (LAN):   useNgrok = false  -> http://192.168.100.120
-  // Emulator:          useEmulator = true -> http://10.0.2.2
+  // Default server settings
   static const bool useNgrok = true;
   static const bool useEmulator = false;
+
   static const String _lanIp = '192.168.100.120';
+
   static const String _ngrokUrl =
       'https://overplant-underling-closure.ngrok-free.dev/photomemories';
 
-  static const String baseUrl = useEmulator
-      ? 'http://10.0.2.2/photomemories'
-      : useNgrok
-          ? _ngrokUrl
-          : 'http://$_lanIp/photomemories';
+  // Server URL entered by the user.
+  static String? _customBaseUrl;
+
+  static String get baseUrl {
+    if (_customBaseUrl != null) {
+      return _customBaseUrl!;
+    }
+
+    return useEmulator
+        ? 'http://10.0.2.2/photomemories'
+        : useNgrok
+            ? _ngrokUrl
+            : 'http://$_lanIp/photomemories';
+  }
+
+  static void setServerIp(String ip) {
+    _customBaseUrl = 'http://$ip/photomemories';
+  }
+
+  static void clearCustomServer() {
+    _customBaseUrl = null;
+  }
 
   static String imageUrl(String filename) => '$baseUrl/uploads/$filename';
 }
